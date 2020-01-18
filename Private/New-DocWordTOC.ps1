c5f577a79d56c781a97e4d4101d9af6a0c33fa28
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2020-01-18 18:31:23 +0100

    Update

--- Private/New-DocWordTOC.ps1
+++
@@ -1,9 +0,0 @@
-﻿function New-DocWordTOC {
-    [CmdletBinding()]
-    param(
-        [Xceed.Document.NET.Container] $WordDocument,
-        [PSCustomObject] $Parameters
-    )
-
-    Add-WordToc -WordDocument $WordDocument -Title $Parameters.Title -Switches $Parameters.Switches -RightTabPos $Parameters.RightTabPos -Supress $True
-}
